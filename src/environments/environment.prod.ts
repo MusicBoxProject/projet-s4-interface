b294ee20dac5bb3d9d8794dd6791f7353134e480
medhabibdhif <mohamed-habib.dhif@imt-atlantique.net>
2018-04-16 22:00:41 +0200

    firebase prod fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyASZOHLPWVcllodk2AMQV_b4BJ_T9ADVdE",
+    authDomain: "projet-s4-interface.firebaseapp.com",
+    databaseURL: "https://projet-s4-interface.firebaseio.com",
+    projectId: "projet-s4-interface",
+    storageBucket: "projet-s4-interface.appspot.com",
+    messagingSenderId: "315306067133"  }
 };
