0224abebef383ddf78060df62db0d7a9eb41d447
medhabibdhif <mohamed-habib.dhif@imt-atlantique.net>
2018-06-24 01:32:51 +0200

    fixing get error2

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express'); 
 const app = express(); 
 
+app.get('*', function(req, res) {
+  res.sendfile('./src/index.html')
+})
 app.use(express.static(__dirname + '/dist')); 
 app.listen(process.env.PORT || 8080);
-app.get('*', function(req, res) {
-    res.sendfile('./src/index.html')
-  })
 //Server
